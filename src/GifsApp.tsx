2277b7895cf61b56bf57031e55a96e0213c53894
luan <luandelossantos@gmail.com>
2026-04-12 06:14:26 -0300

    update

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,44 +1,38 @@
-import { mockGifs } from "./mock-data/gifs.mock"
+import { mockGifs } from "./mock-data/gifs/gifs.mock"
+import { CustomHeader } from "./shared/components/CustomHeader"
+import { SearchBar } from "./shared/components/SearchBar"
+import { PrevSearch } from "./shared/components/PrevSearch"
+import { GifsList } from "./shared/components/GifsList"
+
+
 
 export const GifsApp = () =>{
+
+    const handleTermClick = (term: string) => {
+        console.log({term}),
+    }
+
+
+
+
+    const [previusTerms, setpreviusTerms] = useState(['DRBZ'])
     return (
         <>
-        <div className="content-center">
-            <h1>Buscador de Gifs</h1>
-            <p>Descubre gifs</p>
-        </div>
+        {/* Header */}
+        <CustomHeader title="Buscador de Gifs" description="Descubre y comparte el Gif perfecto"/>
 
 
         {/* Search */}
-        <div className="search-container">
-        <input type="text" placeholder="Buscar gifs"/>
-        <button>Buscar</button>
-        </div>
+        <SearchBar/>
 
         {/* Busquedas previas */}
+        <PrevSearch searches={previusTerms} 
+        onLabelClick={handleTermClick}/>
 
-        <div className="previous-searches">
-            <h2>Busquedas previas</h2>
-            <ul className="previous-searches-list">
-            <li>Goku</li>
-            <li>saitama</li>
-            <li>pene</li>
-            </ul>
-        </div>
+   
 
         {/* Gifs */}
-        <div className="gifs-container">
-            {mockGifs.map((gif)=>(
-                <div key={gif.id} className="gif-card">
-                    <img src={gif.url} alt={gif.title} />
-                    <h3>{gif.title}</h3>
-                    <p>
-                    {gif.width}x{gif.height}(1.5mb)
-                    </p>
-                </div>
-            ))}
-        </div>
-
+        <GifsList gifs={mockGifs}/>
         </>
     )
 }
---
+++ src/shared/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+
+
+ 
+export const SearchBar = () => {
+  return (
+    <div className="search-container">
+        <input type="text" placeholder="Buscar gifs"/>
+        <button>Buscar</button>
+        </div>
+  )
+}
